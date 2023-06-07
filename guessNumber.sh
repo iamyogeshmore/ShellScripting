7ab1971cf98da8c019e108e449b1cdad8013451d
Yogesh More <yogesh.more2098@gmail.com>
2023-06-08 01:49:59 +0530

    Added: Guess number game.

---
+++ guessNumber.sh
@@ -0,0 +1,16 @@
+guess_num=0
+random_num=1
+
+while (( $guess_num != $random_num ))
+do
+     read -p "Guess a number:" guess_num
+     random_num=$(( RANDOM%100 + 1 ))
+     if (($guess_num < $random_num ))
+     then
+           echo "Guess was too low...."
+     elif (( $guess_num != $random_num ))
+     then
+           echo "You guessed too high"
+     else
+            echo   "You Win"
+     fi
